31c262ca4d1cca8254728d78c214aa47a8c12d9a
Dean Friedland <deanfriedland@Deans-Mac.local>
2017-01-22 20:54:31 -0500

    Added border color to flags

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -18,7 +18,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
         countries.append("estonia")
         countries.append("france")
         countries.append("germany")
